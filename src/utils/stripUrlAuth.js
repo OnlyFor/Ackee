2c7db14350b2e8f62c0e581c3cf72808197c098f
Tobias Reich <tobias@electerious.com>
2026-02-21 18:39:28 +0100

    Update normalize-url

--- src/utils/stripUrlAuth.js
+++ src/utils/stripUrlAuth.js
@@ -2,6 +2,7 @@ import normalizeUrl from 'normalize-url'
 
 export default (url) =>
   normalizeUrl(url, {
+    customProtocols: ['mongodb'],
     normalizeProtocol: false,
     stripWWW: false,
     removeTrailingSlash: false,
